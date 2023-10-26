f6db837ce737bc9adff132868da596e2b38e1b60
Steven Gaughran <73718182+StevenGaughran@users.noreply.github.com>
2023-10-26 09:13:50 -0700

    V3 (#33)
    * PyTest Folder
    
    Created the PyTest folder, as per Issue #9.
    
    * 10/5 changes with Scott
    
    * Updated checkboxes
    
    * Cleanup of hunter_data
    
    * Checklist Update
    
    Replaces the listbox with checkbuttons. A work in progress.
    
    * Update hunter_data.cpython-39.pyc
    
    * removed bad files
    
    * Checkboxes
    
    * Checkboxes (again)
    
    * Delete hunter_data.cpython-39.pyc
    
    * Future Setup
    
    Created a placeholder file for future class-oriented GUI structure.
    
    Made some new functions to interact with json data updates.
    
    * Function Writing
    
    Moved some functions from 'main' to 'hunter_data'.
    
    Ran some very poor tests poorly.
    
    Wrote some function code.
    
    Drank a smoothie (it was good).
    
    * Function Movement & CLeanup
    
    Cleaned up tkinter imports.
    
    Moved some functions around, cleaned up others.
    
    * Fixed get_numbers function
    
    * Some tests
    
    Made some tests...WHICH WERE SUCCESSFUL, I MIGHT ADD
    
    * 10/11 edits
    
    * Update .gitignore
    
    * removing pyc files
    
    * removing pyc files
    
    * more work
    
    * 10/12 Update with Scott
    
    Moving more functions to 'hunter_data'
    
    * Running!
    
    The program runs!
    
    Modified lots of the functions and their arguments for clarity.
    
    Created the Button command, which runs the whole dang thing.
    
    Still need to test the JSON update function.
    
    * IT LIVES!
    
    Totally functional!
    
    It now updates the JSON file to keep track!
    
    * working edits
    
    * Documentation and Testing
    
    Added a bunch of documentation to the functions.
    
    Created a few Pytests, still getting the hang of it.

--- hunter_data.py
+++ hunter_data.py
@@ -2,35 +2,68 @@ import json
 import random
 from tkinter import IntVar,Checkbutton,Toplevel,Label,Button
 
-"""The data pulled from 'hunter_list.json', and manipulated into useable form.
-Hunter_list pulls the json data.
-Owned_hunters searches the json data for True values.
-Unowned_hunters searches the json data for False values."""
 def hunter_list():
+    """Loads a list of hunters from the file `hunter_list.json` and returns it.
+
+    Returns:
+        A dictionary of hunters with true/false values.
+        True values indicated "owned" hunters.
+        False values indicate "unowned" hunters.
+    """
     with open("hunter_list.json", "r") as i:
         hunters = json.load(i)
     return hunters
 
-# A list of owned hunters, based on True key values in 'hunter_list.json'
 def owned_hunters(hunter_list=None):
+    """Creates a list of "owned" hunters.
+
+    Args:
+        hunter_list: json data from 'hunter_list()'.
+
+    Returns:
+        A list of all hunter names with a 'true' value.
+    """
     owned = [k for k, v in hunter_list.items() if v == True]
     return owned
 
-# A list of unowned hunters, based on False key values in 'hunter_list.json'
 def unowned_hunters(hunter_list=None):
+    """Creates a list of "unowned" hunters.
+
+    Args:
+        hunter_list: json data from 'hunter_list()'.
+
+    Returns:
+        A list of all hunter names with a 'false' value.
+    """
     unowned = [k for k, v in hunter_list.items() if v == False]
     return unowned
 
 # Creates a list of Hunter names derived from 'hunter_list'
 def hunter_names(hunter_list=None):
+    """Creates a list of all hunter names.
+
+    Args:
+        hunter_list: json data from 'hunter_list()'.
+
+    Returns:
+        A list of all hunter names, derived from 'hunter_list()' keys.
+    """
     h_list = []
     for i in hunter_list:
         h_list.append(i)
     return h_list
 
-"""This fills in the window with hunter names, and pre-selects them if they are 'owned'.
-Populates the hunter_list_numbers with returned IntVars."""
 def populate_hunter_list(text=None):
+    """This fills in the window with checkbuttons and hunter names.
+    Pre-selects them if they are 'owned'.
+    Populates the hunter_list_numbers with returned IntVars.
+
+    Args:
+        text: 'scrolledtext.ScrolledText()' in 'main.py'.
+
+    Returns:
+        A list populated by checkbutton IntVars.
+    """
     hunter_list_numbers = []
     for i in hunter_list():
         if i in owned_hunters(hunter_list()):
@@ -56,40 +89,64 @@ def populate_hunter_list(text=None):
             text.insert('end', '\n')
     return hunter_list_numbers
 
-"""Gets the index numbers from the checkbox IntVars."""
 def get_numbers(hunter_list_numbers=None):
+    """Extracts 0 and 1 IntVar values.
+
+    Args:
+        hunter_list_numbers: IntVars from 'hunter_list_numbers()'.
+
+    Returns:
+        A list populated by IntVar values, either 0 or 1.
+    """
     number_list = []
     for i in hunter_list_numbers:
         x = i.get()
         number_list.append(x)
     return number_list
 
-"""Gets the names necessary for random selection.
-Takes the two lists and creates an index. 
-Uses the index to pull the appropriate names.
-Returns a list of strings."""
 def ugly_child(get_numbers=None, hunter_names=None):
+    """Gets the hunter names necessary for random selection.
+    Takes the two lists and creates an index.
+    Uses the index to pull the appropriate names.
+
+    Args:
+        get_numbers: numbers from 'get_numbers()'.
+        hunter_names: names from 'hunter_names()'.
+
+    Returns:
+        A list names in string format.
+    """
     chosen_ones = []
-    # Find the location of all 1 in checkbox_selection
     for index, element in enumerate(get_numbers):
         if element == 1:
-    # Find the location of i in the hunter_list
             chosen_ones.append(hunter_names[index])
     return chosen_ones
 
-"""The function that randomly selects your Hunter from 'ugly_child'."""
 def random_hunter_selection(ugly_child=None):
+    """Randomly selects a hunter name.
+
+    Args:
+        ugly_child: the list of names to select from randomly.
+
+    Returns:
+        A single name in string format.
+    """
     selection = random.choice(ugly_child)
     return selection
 
-"""Function to open a new window to place the hunter selected from 'random_hunter_selection'.
-The nested function 'close_it' makes it so the button in the new window closes the new window."""
 def top_window(window=None, selection=None):
+    """Creates a new window to place the selected hunter name.
+
+    Args:
+        window: 'window' from 'main.py'.
+        selection: The name chosen from 'random_hunter_selection()'.
+    """
     new_window = Toplevel()
     new_window.title("Your hunter is...")
     new_window.geometry("300x200")
 
     def close_it():
+        """Destroys the top window when the button is pressed."""
         new_window.destroy()
 
     hunter_announce = Label(new_window,text="Your hunter is")
@@ -105,11 +162,16 @@ def top_window(window=None, selection=None):
     )
     close_window.pack()
 
-"""Updates the json database of hunters based on user input.
-Checks the output of 'chosen_ones' against the 'hunter_list'.
-If the names in 'chosen_ones' are in 'hunter_list', makes the appropriate value 'true'.
-Otherwise, makes it 'false'."""
 def update_json(hunter_list=None,ugly_child=None):
+    """Updates the json database of hunters based on checkbox input.
+    Checks the output of 'chosen_ones' against the 'hunter_list'.
+    If the names in 'chosen_ones' are in 'hunter_list', makes the appropriate value 'true'.
+    Otherwise, makes it 'false'.
+
+    Args:
+        hunter_list: names from 'hunter_list()'.
+        ugly_child: names from 'ugly_child()'.
+    """
     data = hunter_list
     for i in data:
         if i in ugly_child:
@@ -119,10 +181,16 @@ def update_json(hunter_list=None,ugly_child=None):
     with open('hunter_list.json', 'w') as f:
         json.dump(data,f)
 
-"""The button that pulls all this nonsense together."""
 def the_button(hunter_list_numbers=None,
                selection_prompt=None,
                window=None):
+    """The button that you press to make this beautiful disaster work.
+
+    Args:
+        hunter_list_numbers: numbers from 'hunter_list_numbers()'.
+        selection_prompt: 'selection_prompt' from 'main.py'.
+        window: 'window' from 'main.py'.
+    """
     chosen_ones = ugly_child(get_numbers=get_numbers(hunter_list_numbers),
                              hunter_names=hunter_names(hunter_list()))
     selection = random_hunter_selection(ugly_child=chosen_ones)
@@ -130,4 +198,3 @@ def the_button(hunter_list_numbers=None,
     selection_prompt.config(text=selection)
     update_json(hunter_list=hunter_list(),
                 ugly_child=chosen_ones)
-
--- test/test_ugly_child.py
+++ test/test_ugly_child.py
@@ -1,7 +1,43 @@
 import pytest
 import random
-from hunter_data import ugly_child, random_hunter_selection
+import json
+from hunter_data import ugly_child, random_hunter_selection, hunter_list
 from tkinter import IntVar
+
+# Four functions that test the 'hunter_list' function.
+@pytest.mark.skip(reason="Passed")
+def test_hunter_list_file_exists():
+    # Check if the 'hunter_list.json' file exists
+    with open("../hunter_list.json", "r") as file:
+        pass  # If the file doesn't exist, an error will be raised here
+
+# Having trouble with the following three functions.
+# They can't seem to find 'hunter_list.json'?
+def test_hunter_list_returns_dict():
+    # Check if the function returns a dictionary
+    result = hunter_list()
+    assert isinstance(result, dict)
+
+def test_hunter_list_dict_structure():
+    # Check the structure of the returned dictionary
+    result = hunter_list()
+    expected_structure = {
+        "hunter1": bool,
+        "hunter2": bool,
+        # Add more keys for other hunters if needed
+    }
+
+    for key, value_type in expected_structure.items():
+        assert key in result
+        assert isinstance(result[key], value_type)
+
+def test_hunter_list_file_contents():
+    # Check if the file contents match the returned dictionary
+    with open("../hunter_list.json", "r") as file:
+        file_data = json.load(file)
+
+    result = hunter_list()
+    assert result == file_data
 
 @pytest.mark.skip
 def test_happy_day():
@@ -34,7 +70,7 @@ def test_get_numbers():
         number_list.append(x)
     assert x == [100]
 
-# @pytest.mark.skip(reason="Success!")
+@pytest.mark.skip(reason="Success!")
 # Change first 'for' loop to look through the big_hunter_list, not chosen_ones.
 def test_update_hunter_list():
     chosen_ones = ["Jan", "Casey"]
@@ -51,7 +87,6 @@ def test_update_hunter_list():
                                "Casey": True,
                                "Scott": False,
                                "Steve": False}
-
 
 @pytest.mark.skip(reason="Works")
 def test_random_selection():
